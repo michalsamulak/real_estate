be09f77f9f02565205f22f6e557418cf4ef52860
Michal Samulak <47996655+michalsamulak@users.noreply.github.com>
2023-04-23 20:45:20 +0200

    added my lisings page

--- src/components/Navbar/Login.tsx
+++ src/components/Navbar/Login.tsx
@@ -9,41 +9,44 @@ export const LoginButton = () => {
     const userContext = useAuthContext();
 
     const isLoggedIn = userContext.user !== null;
-return (
- <>
-    {isLoggedIn ? (
-        <div className={styles.wrapper}>
-            <ul className={styles.navItems}>
-                <Link href={"/"}>
-                    <li
-                        className={styles.navItem}
-                        onClick={logOutHandler}
-                        >
-                        Sign&#8901;out
-                    </li>
-                </Link>
-            </ul>
-        </div>
-    ) : (
-        <div className={styles.wrapper}>
-            <ul className={styles.navItems}>
-                <Link href={"/login"}>
-                    <li className={styles.navItem}>Sign&#8901;in</li>
-                </Link>
-            </ul>
-            <ul className={styles.navItemsBtn}>
-                <Link href={"/join"}>
-                    <li className={styles.navItemBtn}>
-                        <button className={styles.button_join}>
-                            Join
-                        </button>
-                    </li>
-                </Link>
-            </ul>
-        </div>
-    )}
-    </>
-   )
+    return (
+        <>
+            {isLoggedIn ? (
+                <div className={styles.wrapper}>
+                    <ul className={styles.navItems}>
+                        <Link href={"/"}>
+                            <li
+                                className={styles.navItem}
+                                onClick={logOutHandler}
+                            >
+                                Sign&#8901;out
+                            </li>
+                        </Link>
+                        <Link href={"/listed"}>
+                            <li className={styles.navItem}>my listings</li>
+                        </Link>
+                    </ul>
+                </div>
+            ) : (
+                <div className={styles.wrapper}>
+                    <ul className={styles.navItems}>
+                        <Link href={"/login"}>
+                            <li className={styles.navItem}>Sign&#8901;in</li>
+                        </Link>
+                    </ul>
+                    <ul className={styles.navItemsBtn}>
+                        <Link href={"/join"}>
+                            <li className={styles.navItemBtn}>
+                                <button className={styles.button_join}>
+                                    Join
+                                </button>
+                            </li>
+                        </Link>
+                    </ul>
+                </div>
+            )}
+        </>
+    );
     // if (isLoggedIn) {
     //   return (
     //     <Link href={"/"}>
@@ -66,6 +69,4 @@ return (
     //     </>
     //   );
     // }
-  };
-  
-
+};
---
+++ src/pages/listed/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Listed = () => {
+  return (
+    <div>index</div>
+  )
+}
+
+export default Listed
