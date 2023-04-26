6c264c548c61789df1e178581667211e1adfffb1
Michal Samulak <47996655+michalsamulak@users.noreply.github.com>
2023-04-26 11:33:24 +0200

    deploy at vercel

--- src/utils/listingProperty/validation.ts
+++ src/utils/listingProperty/validation.ts
@@ -1,18 +1,17 @@
 import * as Yup from "yup";
 
 export const validationSchema = Yup.object().shape({
-  // title: Yup.string().required(),
-  // num_bedrooms: Yup.string().required(),
-  // bathrooms: Yup.string().required(),
-  // area: Yup.number().required(),
-  // price: Yup.number().required(),
-  // description: Yup.string().required(),
-  // // img: Yup.string().required(),
-  // phone_number: Yup.number().required(),
-  // street: Yup.string().required(),
-  // city: Yup.string().required(),
-  // state: Yup.string().required(),
-  // zip: Yup.string().required(),
-  // country: Yup.string().required(),
+  title: Yup.string().required(),
+  num_bedrooms: Yup.string().required(),
+  bathrooms: Yup.string().required(),
+  area: Yup.number().required(),
+  price: Yup.number().required(),
+  description: Yup.string().required(),
+  phone_number: Yup.number().required(),
+  street: Yup.string().required(),
+  city: Yup.string().required(),
+  state: Yup.string().required(),
+  zip: Yup.string().required(),
+  country: Yup.string().required(),
 });
 
